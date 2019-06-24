96358530ad8c1273dbbec3a6d3a126aaefbf61cf
Nashit <l164104@lhr.nu.edu.pk>
2019-06-24 17:27:49 +0500

    Functionality implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  root "users#index"  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get "signup", to: "sessions#new", as: "signup"
+  get "login" , to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
